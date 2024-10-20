9b2c2866b505964feb1aa7332471f60430d83ede
Spider14Man <sadekhady14@gmail.com>
2024-10-20 18:56:13 +0300

    Fixed issue with instructor and attendee validation

--- models/res_partner.py
+++ models/res_partner.py
@@ -6,15 +6,11 @@ class ResPartner(models.Model):
 
     course_ids = fields.One2many('course.management', 'instructor_id', string='Courses')
     instructor_ids = fields.Many2many(
-        'res.partner',  # Related model
-        'partner_instructor_rel',  # Relation table name
-        'partner_id',  # Column for this model (res.partner)
-        'instructor_partner_id',  # Column for related model (res.partner)
+        'res.partner',
+        'partner_instructor_rel',
+        'partner_id',
+        'instructor_partner_id',
         string='Instructor Name'
     )
-    @api.constrains('instructor_course_ids', 'attendee_course_ids')
-    def _check_instructor_attendee(self):
-        for partner in self:
-            for course in partner.course_ids:
-                if course in partner.instructor_ids:
-                    raise ValidationError("A person cannot be both an instructor and an attendee of the same course.")
+
+   
